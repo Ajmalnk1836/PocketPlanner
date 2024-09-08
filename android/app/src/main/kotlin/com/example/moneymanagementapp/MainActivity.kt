c53f07a56097cf2b493fc157a7f56dec514214ae
ajmalnk <ajmal@detecttechnologies.com>
2024-09-08 22:30:07 +0530

    ff

--- android/app/src/main/kotlin/com/example/moneymanagementapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moneymanagementapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/Utils/mediaquery.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScreenSize {
-  static double screenHeight(BuildContext context, double percentage) {
-    return MediaQuery.of(context).size.height * percentage / 100;
-  }
-}
